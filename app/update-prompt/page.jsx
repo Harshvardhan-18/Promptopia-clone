06f102e8b89df7955aa6efdd1fe7046325038df4
Harshvardhan <harshvar8an@gmail.com>
2025-01-09 16:19:31 +0530

    fixing typo

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -9,7 +9,6 @@ const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
-
   const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
 
